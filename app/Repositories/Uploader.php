adfe34aaaaa5df61821e734e7c8fb5ca6053601c
andela-vdugeri <verem.dugeri@andela.com>
2015-12-12 12:10:19 +0100

    Add file upload helpers

---
+++ app/Repositories/Uploader.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Helpers;
+
+use Cloudder;
+
+class Uploader
+{
+
+	/*
+    * uploads a file to cloudinary using the cloudder helper facade and returns
+    * the result of the upload to the user or false if it fails;
+    */
+   
+   public function upload($file)
+   {
+   		if (isset($ffile)) {
+   			Cloudder::upload($file);
+
+   			return Cloudder::getResult();
+   		}
+
+   		return false;
+   }
+}
---
+++ app/Repositories/UrlShortener.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Helpers;
+
+
+
+class UrlShortener
+{
+
+	private $apiVersion = 'v3';
+
+
+	private $format;
+
+
+	private $login;
+
+	private $apiKey;
+
+
+	public function setKey($key)
+	{
+		$this->apiKey = $key;
+	}
+
+	public function setLogin($login)
+	{
+		$this->login = $login;
+	}
+
+	public function setFormat($format)
+	{
+		$this->format = $format;
+	}
+
+
+	public function shortenUrl($url)
+	{
+		$encodedUrl = urlencode($url);
+		$bitlyUrl = "http://api.bit.ly/shorten?version=".$this->apiVersion."&format=".$this->format."&longUrl=".$encodedUrl."$login=".$this->login."&apiKey=".$this->apiKey;
+
+		$content = file_get_contents($bitlyUrl);
+
+		try {
+			$this->parseContent($content, $url);
+		} catch(Exception $e) {
+			return "Caught exception ". $e->getMessage()."."
+		}
+	}
+
+
+	private function parseUrl($url)
+	{
+		$parsedUrl = parse_url($url);
+
+		return trim($parsedUrl['path'], '/');
+	}
+
+
+	public function parseContent($content, $key)
+	{
+		$content = json_decode($content);
+		$shortur = $content['results'][$key]['shortUrl'];
+
+		if ($content['statusCode'] != 'OK') {
+			return $content['statusCode'].":"$content['errorCode'].": ".$content['errorMessage'];
+		}
+
+		return (isset($shprtUrl))? $shortUrl : "Error: Url not found ".$key;
+	}
+
+}
