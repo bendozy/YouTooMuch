bfd1db86927976fcd362a4fda207ce3ee8aedec0
andela-vdugeri <verem.dugeri@andela.com>
2015-12-13 23:34:01 +0100

    Reformat code

--- app/Repositories/Uploader.php
+++ app/Repositories/Uploader.php
@@ -5,20 +5,19 @@ use Cloudder;
 
 class Uploader
 {
-
-	/*
+    /*
     * uploads a file to cloudinary using the cloudder helper facade and returns
     * the result of the upload to the user or false if it fails;
     */
    
    public function upload($file)
    {
-   		if (isset($file)) {
-   			Cloudder::upload($file);
+       if (isset($file)) {
+           Cloudder::upload($file);
 
-   			return Cloudder::getResult();
-   		}
+           return Cloudder::getResult();
+       }
 
-   		return false;
+       return false;
    }
 }
