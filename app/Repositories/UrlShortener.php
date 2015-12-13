a2f68c7dd46ea0c447b3184574ee872d7857d0c0
andela-vdugeri <verem.dugeri@andela.com>
2015-12-13 23:43:46 +0100

    Fix indentation

--- app/Repositories/UrlShortener.php
+++ app/Repositories/UrlShortener.php
@@ -4,65 +4,65 @@ namespace App\Helpers;
 
 class UrlShortener
 {
-	/**
+	 /**
 	 * bit.ly api version
 	 * @var string
 	 */
-    private $apiVersion = 'v3';
+     private $apiVersion = 'v3';
 
-    /**
+     /**
      * request format
      * @var string
      */
-    private $format;
+     private $format;
 
-    /**
+     /**
      * bit.ly api login key
      * @var string
      */
-    private $login;
+     private $login;
 
-    /**
+     /**
      * bit.ly api key
      * @var [type]
      */
-    private $apiKey;
+     private $apiKey;
 
 
-    /**
+     /**
      * 
      * @param string $key 
      */
-    public function setKey($key)
-    {
+     public function setKey($key)
+     {
         $this->apiKey = $key;
-    }
+     }
 
-    /**
+     /**
      * 
      * @param string $login 
      */
-    public function setLogin($login)
-    {
+     public function setLogin($login)
+     {
         $this->login = $login;
-    }
+     }
 
-    /**
+     /**
      * 
      * @param string $format
      */
-    public function setFormat($format)
+     public function setFormat($format)
     {
         $this->format = $format;
     }
 
-    /**
+     /**
      * 
      * @param  string $url 
      * @return mixed
      */
-    public function shortenUrl($url)
-    {
+     public function shortenUrl($url)
+     {
         $encodedUrl    = urlencode($url);
 
         $bitlyUrl        = "http://api.bit.ly/shorten?version=".$this->apiVersion."&format=".$this->format."&longUrl=".$encodedUrl."&login=".$this->login."&apiKey=".$this->apiKey;
@@ -74,27 +74,27 @@ class UrlShortener
         } catch (Exception $e) {
             return "Caught exception ". $e->getMessage().".";
         }
-    }
+     }
 
-    /**
+     /**
      * 
      * @param  string $url 
      * @return string
      */
-    private function parseUrl($url)
-    {
+     private function parseUrl($url)
+     {
         $parsedUrl        = parse_url($url);
         return trim($parsedUrl['path'], '/');
-    }
+     }
 
-    /**
+     /**
      * 
      * @param  string $content 
      * @param  string $key
      * @return string
      */
-    public function parseContent($content, $key)
-    {
+     public function parseContent($content, $key)
+     {
         $content    = json_decode($content, true);
 
         if ($content['statusCode'] != 'OK') {
@@ -103,5 +103,5 @@ class UrlShortener
 
         $shortUrl    = $content['results'][$key]['shortUrl'];
         return (isset($shortUrl))? $shortUrl : "Error: Url not found ".$key;
-    }
+     }
 }
