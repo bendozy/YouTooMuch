2f845cc9628fc43b2e27e4212ef5b46a0e28b146
andela-vdugeri <verem.dugeri@andela.com>
2015-12-13 23:39:30 +0100

    Add doc block

--- app/Repositories/UrlShortener.php
+++ app/Repositories/UrlShortener.php
@@ -4,33 +4,63 @@ namespace App\Helpers;
 
 class UrlShortener
 {
+	/**
+	 * bit.ly api version
+	 * @var string
+	 */
     private $apiVersion = 'v3';
 
-
+    /**
+     * request format
+     * @var string
+     */
     private $format;
 
-
+    /**
+     * bit.ly api login key
+     * @var string
+     */
     private $login;
 
+    /**
+     * bit.ly api key
+     * @var [type]
+     */
     private $apiKey;
 
 
+    /**
+     * 
+     * @param string $key 
+     */
     public function setKey($key)
     {
         $this->apiKey = $key;
     }
 
+    /**
+     * 
+     * @param string $login 
+     */
     public function setLogin($login)
     {
         $this->login = $login;
     }
 
+    /**
+     * 
+     * @param string $format
+     */
     public function setFormat($format)
     {
         $this->format = $format;
     }
 
-
+    /**
+     * 
+     * @param  string $url 
+     * @return mixed
+     */
     public function shortenUrl($url)
     {
         $encodedUrl    = urlencode($url);
@@ -46,14 +76,23 @@ class UrlShortener
         }
     }
 
-
+    /**
+     * 
+     * @param  string $url 
+     * @return string
+     */
     private function parseUrl($url)
     {
         $parsedUrl        = parse_url($url);
         return trim($parsedUrl['path'], '/');
     }
 
-
+    /**
+     * 
+     * @param  string $content 
+     * @param  string $key
+     * @return string
+     */
     public function parseContent($content, $key)
     {
         $content    = json_decode($content, true);
