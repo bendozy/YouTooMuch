8c768b279c8efef0f6bc09f9c9300e4d3e127fba
Oduye Oluwayemisi <oduyeoluwayemisi@Oduyes-MacBook-Pro.local>
2015-12-14 18:45:35 +0100

    Added the basic application frontend layout

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; U2Much 2015</p>
+            </div>
+        </div>
+    </footer>
+
+</div>
