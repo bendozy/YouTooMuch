d7d8fdb8f90929aef61fbc2e79b95c9f1ee7909b
andela-vdugeri <verem.dugeri@andela.com>
2015-12-14 10:21:09 +0100

    Resolve merge conflicts

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -10,19 +10,27 @@ use App\Http\Controllers\Controller;
 class UploadController extends Controller
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 
 <<<<<<< HEAD
+=======
+
+
+>>>>>>> 18a3b9e... Add docblock
 
     /**
      * @param  Request     $request  
      * @param  UploadImage $uploader 
      * @return mixed
      */
+<<<<<<< HEAD
 =======
 >>>>>>> e95f74d... Complete image upload feature with test file
 =======
 >>>>>>> bfd1db8... Reformat code
+=======
+>>>>>>> 18a3b9e... Add docblock
     public function uplaod(Request $request, UploadImage $uploader)
     {
         if ($request->ajax()) {
--- app/Repositories/UploadImage.php
+++ app/Repositories/UploadImage.php
@@ -4,55 +4,53 @@ namespace App\Helpers;
 
 class UploadImage
 {
-    /**
+     /**
      * UrlShortener object reference
      * @var UrlShortener
      */
-    private $shortener;
-<<<<<<< HEAD
+      private $shortener;
 
-<<<<<<< HEAD
-    /**
+     /**
      * Uploader object reference.
      * @var Uploader
      */
-    private $uploader;
+     private $uploader;
 
-    /**
+     /**
      * Shortened url of uploaded file
      * @var string
      */
-    private $shortUrl;
+     private $shortUrl;
 
-    /**
+     /**
      * Initialise the object
      * @param Uploader     $uploader  
      * @param UrlShortener $shortener 
      */
-    public function __construct(Uploader $uploader, UrlShortener $shortener)
-    {
+     public function __construct(Uploader $uploader, UrlShortener $shortener)
+     {
         $this->shortener    = $shortener;
         $this->uploader    = $uploader;
-    }
+     }
 
-    /**
+     /**
      * Initialise bitly url shortenin service configs
      * @return [type] [description]
      */
-    public function intConfig()
-    {
+     public function intConfig()
+     {
         $this->shortener->setLogin(env('BITLY_LOGIN'));
         $this->shortener->setKey(env('BITLY_API_KEY'));
         $this->shortener->setFormat('json');
-    }
+     }
 
-    /**
+     /**
      * Upload image to cloudinary and return the shortened url
      * @param  Image $image 
      * @return string        shortened url
      */
-    public function upload($image)
-    {
+     public function upload($image)
+     {
         $this->intConfig();
 
         if (! is_null($image)) {
@@ -60,101 +58,25 @@ class UploadImage
             $longUrl    = $result['url'];
             $this->shortUrl =   $this->shortenUrl($longUrl);
         }
-    }
-=======
-	/**
-	 * UrlShortener object reference
-	 * @var UrlShortener
-	 */
-	private $shortener;
+     }
 
-	/**
-	 * Uploader object reference.
-	 * @var Uploader
-	 */
-	private $uploader;
-=======
-
-    /**
-     * Uploader object reference.
-     * @var Uploader
-     */
-    private $uploader;
->>>>>>> bfd1db8... Reformat code
-
-    /**
-     * Shortened url of uploaded file
-     * @var string
-     */
-    private $shortUrl;
-
-    /**
-     * Initialise the object
-     * @param Uploader     $uploader  
-     * @param UrlShortener $shortener 
-     */
-    public function __construct(Uploader $uploader, UrlShortener $shortener)
-    {
-        $this->shortener    = $shortener;
-        $this->uploader    = $uploader;
-    }
-
-    /**
-     * Initialise bitly url shortenin service configs
-     * @return [type] [description]
-     */
-    public function intConfig()
-    {
-        $this->shortener->setLogin(env('BITLY_LOGIN'));
-        $this->shortener->setKey(env('BITLY_API_KEY'));
-        $this->shortener->setFormat('json');
-    }
-
-    /**
-     * Upload image to cloudinary and return the shortened url
-     * @param  Image $image 
-     * @return string        shortened url
-     */
-    public function upload($image)
-    {
-        $this->intConfig();
-
-<<<<<<< HEAD
-		if (! is_null($image)) {
-			$result 	= $this->uploader->upload($image);
-			$longUrl 	= $result['url'];
-			$this->shortUrl =   $this->shortenUrl($longUrl);
-		}
-	}
->>>>>>> e95f74d... Complete image upload feature with test file
-=======
-        if (! is_null($image)) {
-            $result    = $this->uploader->upload($image);
-            $longUrl    = $result['url'];
-            $this->shortUrl =   $this->shortenUrl($longUrl);
-        }
-    }
->>>>>>> bfd1db8... Reformat code
-
-    /**
+     /**
      * Shorten the file url
      * @param  string $longUrl 
      * @return string          the shortened url
      */
-    public function shortenUrl($longUrl)
-    {
+     public function shortenUrl($longUrl)
+     {
         return $this->shortener->shortenUrl($longUrl);
-    }
+     }
 
-<<<<<<< HEAD
-    /**
+     /**
      * 
      * @return string
      */
-=======
->>>>>>> bfd1db8... Reformat code
-    public function getShortUrl()
-    {
+
+     public function getShortUrl()
+     {
         return $this->shortUrl;
-    }
+     }
 }
