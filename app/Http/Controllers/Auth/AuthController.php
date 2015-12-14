73eb9db1ad55ba031f664a1ef9b0aef13fda3fbc
Dara Oladosu <daradosu@gmail.com>
2015-12-14 14:29:02 +0100

    Enable facebook Auth

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use Socialite;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
@@ -21,45 +24,68 @@ class AuthController extends Controller
     |
     */
 
+        /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
+
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    protected $redirectPath = '/home';
+
+
+    public function facebookLogin()
     {
-        $this->middleware('guest', ['except' => 'getLogout']);
+        $driver = 'facebook';
+        return Socialite::driver($driver)->redirect();
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
+    public function handleProviderCallback()
     {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
-        ]);
-    }
+        $driver = 'facebook';
 
+        try {
+            $user = Socialite::driver($driver)->user();
+        } catch (Exception $e) {
+            return redirect('/error');
+        }
+ 
+        $authUser = $this->findOrCreateUser($user, $driver);
+        dd($authUser);
+        Auth::login($authUser, true);
+ 
+        return redirect($this->redirectPath());
+    }
+ 
     /**
-     * Create a new user instance after a valid registration.
+     * Return user if exists; create and return if doesn't
      *
-     * @param  array  $data
+     * @param $socialUser
      * @return User
      */
-    protected function create(array $data)
-    {
+    private function findOrCreateUser($socialUser, $driver)
+    {   
+        $authUser = User::where('facebook_id', $socialUser->id)->first();
+ 
+        if ($authUser){
+            return $authUser;
+        }
+        //dd($socialUser);
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+            'username' => $socialUser->name,
+            'facebook_id' => $socialUser->id,
+            'avatar' => $socialUser->avatar
         ]);
+    }
+
+    protected function logout()
+    {   
+        Auth::logout();
     }
 }
