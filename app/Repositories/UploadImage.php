ca2098906e834d93f07c25ccbb1734ae5b1e1d7c
andela-vdugeri <verem.dugeri@andela.com>
2015-12-14 11:11:33 +0100

    Complete image upload to cloudinary

---
+++ app/Repositories/UploadImage.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Helpers;
+
+class UploadImage
+{
+    /**
+     * UrlShortener object reference
+     * @var UrlShortener
+     */
+      private $shortener;
+
+     /**
+     * Uploader object reference.
+     * @var Uploader
+     */
+     private $uploader;
+
+     /**
+     * Shortened url of uploaded file
+     * @var string
+     */
+     private $shortUrl;
+
+     /**
+     * Initialise the object
+     * @param Uploader     $uploader  
+     * @param UrlShortener $shortener 
+     */
+     public function __construct(Uploader $uploader, UrlShortener $shortener)
+     {
+         $this->shortener    = $shortener;
+         $this->uploader    = $uploader;
+     }
+
+     /**
+     * Initialise bitly url shortenin service configs
+     * @return [type] [description]
+     */
+     public function intConfig()
+     {
+         $this->shortener->setLogin(env('BITLY_LOGIN'));
+         $this->shortener->setKey(env('BITLY_API_KEY'));
+         $this->shortener->setFormat('json');
+     }
+
+     /**
+     * Upload image to cloudinary and return the shortened url
+     * @param  Image $image 
+     * @return string        shortened url
+     */
+     public function upload($image)
+     {
+         $this->intConfig();
+
+         if (! is_null($image)) {
+             $result    = $this->uploader->upload($image);
+             $longUrl    = $result['url'];
+             $this->shortUrl =   $this->shortenUrl($longUrl);
+         }
+     }
+
+     /**
+     * Shorten the file url
+     * @param  string $longUrl 
+     * @return string          the shortened url
+     */
+     public function shortenUrl($longUrl)
+     {
+         return $this->shortener->shortenUrl($longUrl);
+     }
+
+     /**
+     * 
+     * @return string
+     */
+
+     public function getShortUrl()
+     {
+         return $this->shortUrl;
+     }
+}
