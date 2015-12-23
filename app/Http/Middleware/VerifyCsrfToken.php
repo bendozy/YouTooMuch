f82113b4e8d2a74110f6ec5d751685626c76db30
Chidozie Ijeomah <chidozie.ijeomah@andela.com>
2015-12-23 10:51:41 +0100

    Added app /

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        'app'/,
+        'app/',
         'app/templates',
         'upload'
     ];
